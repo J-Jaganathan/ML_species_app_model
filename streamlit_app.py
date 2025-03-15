c6bf962964befcc5997ed4cbd3f4d1368ff5e661
Jaganathan J <162098021+J-Jaganathan@users.noreply.github.com>
2025-03-15 08:23:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
@@ -75,3 +76,7 @@ df_prediction_proba = pd.DataFrame(prediction_proba)
 df_prediction_proba.columns = ['Adelie','Chinstrap','Gentoo']
 df_prediction_proba.rename(columns={0: 'Adelie', 1:'Chinstrap', 2: 'Gentoo'})
 df_prediction_proba
+
+st.subheader('Predicted Species')
+penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
+st.success(str[penguins_species[prediction[0]])
